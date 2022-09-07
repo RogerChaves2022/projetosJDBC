52c38a2714fcf53605538654732e734bf24426aa
Roger Chaves <roger.chaves.rs2@gmail.com>
2022-09-07 10:14:37 -0300

    Curso REACT + SPRING

---
+++ src/main/java/br/com/roger/ProjetoFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package br.com.roger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFinanceiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/roger/ProjetoFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.roger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
