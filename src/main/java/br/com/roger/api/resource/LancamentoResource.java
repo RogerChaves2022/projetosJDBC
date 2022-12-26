b7c0564b65e2cab02f72cdb78bd10ccb5960acb3
Roger Chaves <roger.chaves.rs@gmail.com>
2022-12-26 20:03:45 -0300

    teste

--- src/main/java/br/com/roger/api/resource/LancamentoResource.java
+++ src/main/java/br/com/roger/api/resource/LancamentoResource.java
@@ -2,8 +2,10 @@ package br.com.roger.api.resource;
 
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +27,7 @@ import br.com.roger.service.LancamentoService;
 import br.com.roger.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 
+@CrossOrigin(origins = "https://minhas-financas-production.up.railway.app/*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/lancamentos")
 @RequiredArgsConstructor
