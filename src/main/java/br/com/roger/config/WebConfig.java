031a747698bdff0e80cd78580fe0b9b9b16825c8
Roger Chaves <roger.chaves.rs@gmail.com>
2022-12-26 17:17:19 -0300

    origins

--- src/main/java/br/com/roger/config/WebConfig.java
+++ src/main/java/br/com/roger/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("*")
+            .allowedOrigins("minhas-financas-production.up.railway.app/*")
             .allowedMethods("PUT", "DELETE","POST","GET").allowedHeaders("*").allowCredentials(true);
     }
 }
