38d4c06f421e2100d366c672437c6ea61107921b
Roger Chaves <roger.chaves.rs@gmail.com>
2022-12-27 11:24:55 -0300

    teste 202454

--- src/main/java/br/com/roger/config/WebConfig.java
+++ src/main/java/br/com/roger/config/WebConfig.java
@@ -12,6 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        .allowedMethods("GET","PUT","POST","DELETE","OPTIONS");
+        .allowedMethods("GET","PUT","POST","DELETE","OPTIONS")
+        .allowedOrigins("https://minhas-financas-production.up.railway.app");
     }
 }
