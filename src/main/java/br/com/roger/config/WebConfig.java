6ccb19ef02eb3c3300938a7e54defdd98f10b8f4
Roger Chaves <roger.chaves.rs@gmail.com>
2023-01-13 09:01:26 -0300

    ajuste security

--- src/main/java/br/com/roger/config/WebConfig.java
+++ src/main/java/br/com/roger/config/WebConfig.java
@@ -13,6 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
         .allowedMethods("GET","PUT","POST","DELETE","OPTIONS")
-        .allowedOrigins("*").allowCredentials(true).allowedOriginPatterns("*");
+        .allowedOrigins("*");
     }
 }
